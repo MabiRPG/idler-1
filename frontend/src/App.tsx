fc49f26f781c4fa953409b44a0447ffd579fcac1
Kenneth Lam <kenneth.lam1997@hotmail.com>
2026-04-04 15:21:06 +0800

    frontend: cleaned up unused code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,10 @@
 import React from 'react';
-// import { Provider } from 'react-redux';
-// import { store } from './store/store';
-// import { Login } from './pages/account/login/Login';
-// import { Navigate, Route, Routes } from 'react-router';
-// import { AccountLayout } from './layouts/account/AccountLayout';
 
 import '@/assets/css/global.css';
-import { ConfigProvider } from 'antd';
-// import { DefaultLayout } from './layouts/default/DefaultLayout';
-// import { ProtectedRoute } from './components/ProtectedRoute';
-// import { ROUTES } from './resources/routes-constants';
-// import { Dashboard } from './pages/dashboard/Dashboard';
-// import { PageNotFound } from './pages/pageNotFound/PageNotFound';
 
 const App: React.FC = () => {
   return (
     <></>
-    // <Provider store={store}>
-    //   <ConfigProvider theme={{ token: { fontFamily: 'Figtree' } }}>
-    //     <Routes>
-    //       <Route element={<AccountLayout />}>
-    //         <Route path={ROUTES.HOMEPAGE} element={<Login />}></Route>
-    //       </Route>
-    //       <Route
-    //         element={
-    //           <ProtectedRoute>
-    //             <DefaultLayout />
-    //           </ProtectedRoute>
-    //         }
-    //       >
-    //         <Route path={ROUTES.DASHBOARD}>
-    //           <Route index element={<Dashboard />}></Route>
-    //         </Route>
-    //       </Route>
-    //       <Route element={<AccountLayout />}>
-    //         <Route path="*" element={<PageNotFound />}></Route>
-    //       </Route>
-    //     </Routes>
-    //   </ConfigProvider>
-    // </Provider>
   );
 };
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
-// import { AuthKitProvider } from '@workos-inc/authkit-react';
-// import * as serviceWorker from './serviceWorker';
-// import { BrowserRouter } from 'react-router-dom';
 
-const root = createRoot(document.getElementById('root')!); // createRoot(container!) if you use TypeScript
+const root = createRoot(document.getElementById('root')!);
 root.render(
-  // <AuthKitProvider clientId={import.meta.env.VITE_WORKOS_CLIENT_ID} devMode={true}>
-  //   <BrowserRouter>
       <App />
-    // </BrowserRouter>
-  // </AuthKitProvider>,
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
